b6f500982ee517d633cb33bcaee4dfb4432c10e5
ilyoss <ily.trevino@live.com>
2019-06-02 19:37:19 -0500

    weather-app start, utils folder added

---
+++ app.js
@@ -0,0 +1,12 @@
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+geocode('New York', (error, data) => {
+  console.log('Error', data)
+  console.log('Data', data)
+})
+
+forecast(-75.7088, 44.1545, (error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
+})
