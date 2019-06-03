3b582da382c582d0fd2483e1e69a88b6f4e70da4
ilyoss <ily.trevino@live.com>
2019-06-02 20:51:48 -0500

    Property shorthand and deconstructing

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+//Getting adress from command line
+const address = process.argv[2]
 
-geocode('New York', (error, data) => {
-  console.log('Error', data)
-  console.log('Data', data)
-})
-
-forecast(-75.7088, 44.1545, (error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
-})
+if(!address){
+  console.log('Please provide an address')
+} else {
+  geocode(address, (error, { latitude, longitude, location }) => {
+    if(error){
+      return console.log(error)
+    }
+    forecast(latitude, longitude, (error, forecastData) => {
+      if(error){
+        return console.log(error)
+      }
+      console.log(location)
+      console.log(forecastData)
+    })
+  })
+}
--- utils/geocode.js
+++ utils/geocode.js
@@ -4,16 +4,16 @@ const geocode = (address, callback) => {
   const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +
   '.json?access_token=pk.eyJ1IjoiaWx5b3NzIiwiYSI6ImNqdmdxOWR2NjBhaWQ0M3E4eWw2aGJpM3AifQ.EXgHLqbNYFBMdiFgVFen5Q&limit=1'
 
-  request({ url: url, json: true}, (error, response) => {
+  request({ url, json: true}, (error, { body }) => {
     if (error) {
       callback('Unnable to connect to location services!', undefined)
-    } else if (response.body.features.length === 0){
+    } else if (body.features.length === 0){
       callback('Unable to find location. Try another search.', undefined)
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name
       })
     }
   })
